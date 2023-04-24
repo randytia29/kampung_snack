0e95c1664aeb44c7519050825533c4ddae4dfc03
Randytia Akbar <randytia.akbar@gmail.com>
2023-04-24 19:27:14 +0700

    detail web

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,12 +1,8 @@
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 
 import 'package:kampung_snack/features/participant/domain/entities/participant.dart';
-import 'package:kampung_snack/theme_manager/space_manager.dart';
-
-import '../theme_manager/asset_manager.dart';
+import 'package:kampung_snack/widgets/detail_web_page.dart';
+import '../widgets/detail_mobile_page.dart';
 
 class DetailScreen extends StatelessWidget {
   const DetailScreen({super.key, required this.participant});
@@ -15,57 +11,16 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final image = participant.imgByte;
-
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            80.0.spaceY,
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Card(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: 150,
-                      height: 150,
-                      margin: const EdgeInsets.all(16),
-                      child: image == null
-                          ? Image.asset(
-                              AssetManager.noImage,
-                              fit: BoxFit.cover,
-                            )
-                          : kIsWeb
-                              ? Image.network(
-                                  image,
-                                  fit: BoxFit.cover,
-                                )
-                              : Image.memory(
-                                  Uint8List.fromList(image.codeUnits),
-                                  fit: BoxFit.cover,
-                                ),
-                    ),
-                    8.0.spaceX,
-                    Padding(
-                      padding: const EdgeInsets.only(top: 24),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(participant.name ?? '-'),
-                          8.0.spaceY,
-                          Text(participant.address ?? '-'),
-                          8.0.spaceY,
-                          Text(participant.job ?? '-')
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxWidth <= 600) {
+              return DetailMobilePage(participant: participant);
+            } else {
+              return DetailWebPage(participant: participant);
+            }
+          },
         ),
       ),
     );
---
+++ lib/widgets/detail_mobile_page.dart
@@ -0,0 +1,71 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:kampung_snack/theme_manager/space_manager.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+import '../features/participant/domain/entities/participant.dart';
+import '../theme_manager/asset_manager.dart';
+
+class DetailMobilePage extends StatelessWidget {
+  const DetailMobilePage({
+    super.key,
+    required this.participant,
+  });
+
+  final Participant participant;
+
+  @override
+  Widget build(BuildContext context) {
+    final image = participant.imgByte;
+
+    return Column(
+      children: [
+        80.0.spaceY,
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 150,
+                  height: 150,
+                  margin: const EdgeInsets.all(16),
+                  child: image == null
+                      ? Image.asset(
+                          AssetManager.noImage,
+                          fit: BoxFit.cover,
+                        )
+                      : kIsWeb
+                          ? Image.network(
+                              image,
+                              fit: BoxFit.cover,
+                            )
+                          : Image.memory(
+                              Uint8List.fromList(image.codeUnits),
+                              fit: BoxFit.cover,
+                            ),
+                ),
+                8.0.spaceX,
+                Padding(
+                  padding: const EdgeInsets.only(top: 24),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(participant.name ?? '-'),
+                      8.0.spaceY,
+                      Text(participant.address ?? '-'),
+                      8.0.spaceY,
+                      Text(participant.job ?? '-')
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
